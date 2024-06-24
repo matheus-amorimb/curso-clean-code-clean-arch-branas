5149382161f011b99aa374df2742eabb8863aabe
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-24 20:49:58 -0300

    :sparkles: create new tests for use case signup

--- RideApp/src/RideApp.Domain/UseCases/SignUp.cs
+++ RideApp/src/RideApp.Domain/UseCases/SignUp.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using RideApp.Domain.Entities;
 using RideApp.Domain.Interfaces;
 
@@ -13,8 +14,40 @@ public class SignUp
 
     public async Task<Guid> Execute(Account account)
     {
+        ValidateEmail(account.Email);
+        ValidateEmailIsNotInUse(account.Email);
+        ValidateName(account.Name);
+        ValidateCpf(account.Cpf);
+        // if (account.IsDriver) ValidateCarPlate(account.CarPlate);
         var accountCreated = await _accountRepository.Create(account);
         return accountCreated.Id;
     }
+
+
+    private void ValidateEmail(string accountEmail)
+    {
+        var emailPattern = @"^[\w-\.]+\@([\w-]+\.)+[\w-]{2,4}$";
+        if(!Regex.IsMatch(accountEmail, emailPattern)) throw new ArgumentException("Email invalid.");
+    }
     
+    private async void ValidateEmailIsNotInUse(string accountEmail)
+    {
+        var existingAccount = await _accountRepository.GetByEmail(accountEmail);
+        if (existingAccount is not null) throw new ArgumentException("Email already in use.");
+    }
+
+    private void ValidateName(string accountName)
+    {
+        var namePattern = @"^[a-zA-Z]+ [a-zA-Z]+$";
+        if(!Regex.IsMatch(accountName, namePattern)) throw new ArgumentException("Name invalid.");
+    }
+    
+    private void ValidateCpf(string accountCpf)
+    {
+        throw new NotImplementedException();
+    }
+    private void ValidateCarPlate(string accountCarPlate)
+    {
+        // var
+    }
 }
