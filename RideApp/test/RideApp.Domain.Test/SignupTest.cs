45e7d8b4a01d0e35832a668fe343b8da681f332f
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-24 20:50:54 -0300

    :sparkles: create new validations for usecase signup

--- RideApp/test/RideApp.Domain.Test/SignupTest.cs
+++ RideApp/test/RideApp.Domain.Test/SignupTest.cs
@@ -5,6 +5,7 @@ using Moq;
 using RideApp.Data.Context;
 using RideApp.Domain.Entities;
 using RideApp.Domain.Interfaces;
+using RideApp.Domain.Test.Builders;
 using RideApp.Domain.UseCases;
 using Xunit;
 
@@ -28,8 +29,6 @@ public class SignupTest : IDisposable
         _dbContext = _serviceProvider.GetRequiredService<AppDbContext>();
         _signUp = _serviceProvider.GetRequiredService<SignUp>();
         _dbContext.Database.EnsureCreated();
-        // _mock = new Mock<IAccountRepository>();
-        // _signUp = new SignUp(_mock.Object);
     }
     
     [Fact]
@@ -38,8 +37,8 @@ public class SignupTest : IDisposable
         var accountExpected = new
         {
             Id = Guid.NewGuid(),
-            Name = "Matheus",
-            Email = "matheus@gmail.com",
+            Name = "Matheus Batista",
+            Email = "matheus123@gmail.com",
             Cpf = "12345678900",
             CarPlate = "ABC-1234",
             IsPassenger = false,
@@ -49,24 +48,52 @@ public class SignupTest : IDisposable
         var account = new Account(accountExpected.Id, accountExpected.Name, accountExpected.Email, accountExpected.Cpf, accountExpected.CarPlate, accountExpected.IsPassenger, accountExpected.IsDriver);
         var accountCreatedId = await _signUp.Execute(account);
         accountExpected.Should().NotBeNull();
-        // _mock.Setup(repository => repository.Create(account)).ReturnsAsync(account);
-        // var accountCreatedId = await _signUp.Execute(account);
-        // _mock.Verify(r => r.Create(It.Is<Account>(account1 => 
-        //     account1.Id == accountExpected.Id && 
-        //     account1.Name == accountExpected.Name && 
-        //     account1.Email == accountExpected.Email && 
-        //     account1.Cpf == accountExpected.Cpf &&
-        //     account1.CarPlate == accountExpected.CarPlate &&
-        //     account1.IsDriver == accountExpected.IsDriver &&
-        //     account1.IsPassenger == accountExpected.IsPassenger
-        //     )
-        // ));
-        // accountCreatedId.Should().Be(accountExpected.Id);
+    }
+
+    [Fact]
+    public async void Signup_WithEmailInUse_ThrowsAnException()
+    {
+        var account1 = AccountBuilder.New().Build();
+        var accountWithEmailInUse = AccountBuilder.New().Build();
+        await _signUp.Execute(account1);
+        Func<Task> action = async() => await _signUp.Execute(accountWithEmailInUse);
+        await action.Should().ThrowAsync<ArgumentException>().WithMessage("Email already in use.");
+    }
+
+    [Fact]
+    public async void SignUp_WithAnInvalidName_ThrowsAnException()
+    {
+        var invalidName = "Joe4 Doe";
+        var account = AccountBuilder.New().WithName(invalidName).Build();
+        Func<Task> action = async() => await _signUp.Execute(account);
+        await action.Should().ThrowAsync<ArgumentException>().WithMessage("Name invalid.");
+    }    
+    
+    [Fact]
+    public async void SignUp_WithAnInvalidEmail_ThrowsAnException()
+    {
+        var invalidEmail = "jose@";
+        var account = AccountBuilder.New().WithEmail(invalidEmail).Build();
+        Func<Task> action = async() => await _signUp.Execute(account);
+        await action.Should().ThrowAsync<ArgumentException>().WithMessage("Email invalid.");
+    }
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData("000.000.000-00")]
+    [InlineData("085.432.125-56")]
+    public async void SignUp_WithAnInvalidCpf_ThrowsAnException(string invalidCpf)
+    {
+        var account = AccountBuilder.New().WithCpf(invalidCpf).Build();
+        Func<Task> action = async() => await _signUp.Execute(account);
+        await action.Should().ThrowAsync<ArgumentException>().WithMessage("Cpf invalid.");
     }
 
 
     public void Dispose()
     {
+        _dbContext.Accounts.RemoveRange(_dbContext.Accounts);
+        _dbContext.SaveChanges();
         _dbContext.Dispose();
     }
 }
