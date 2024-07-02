ea8f16605e12ad60561ebe2e7a55eaeedb899a95
Matheus Batista <mbatista.sarti@gmail.com>
2024-07-01 22:30:53 -0300

    :sparkles: test for usecase RequestRide

---
+++ RideApp/test/RideApp.Domain.Test/RequestRideTest.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+using RideApp.Domain.Entities;
+using RideApp.Domain.UseCases;
+using RideApp.Domain.ValueObjects;
+using Xunit;
+
+namespace RideApp.Domain.Test;
+
+public class RequestRideTest
+{
+    [Fact]
+    public void RequestRide_Successfully()
+    {
+        var from = new Distance(40.712776, -74.005974);
+        var to = new Distance(40.802606, -74.005974);
+        var ride = new Ride(Guid.NewGuid(), from, to);
+        var requestRide = new RequestRide();
+        var rideId = requestRide.Execute(ride);
+        rideId.Should().NotBeEmpty();
+    }
+    
+}
