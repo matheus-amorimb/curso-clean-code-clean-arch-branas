dc2c891b551aab232fd37c115d4c77b28f2e5192
Matheus Batista <mbatista.sarti@gmail.com>
2024-07-10 08:17:44 -0300

    :sparkles: create usecase RequestRide

--- RideApp/src/RideApp.Domain/UseCases/RequestRide.cs
+++ RideApp/src/RideApp.Domain/UseCases/RequestRide.cs
@@ -1,14 +1,17 @@
 using RideApp.Domain.Entities;
 using RideApp.Domain.Interfaces;
+using RideApp.Domain.ValueObjects;
 
 namespace RideApp.Domain.UseCases;
 
-public class RequestRide(IRideRepository rideRepository) : IUseCase<Ride, Guid>
+public class RequestRide(IRideRepository rideRepository)
 {
     private readonly IRideRepository _rideRepository = rideRepository;
+    public Ride ride { get; set; }
 
-    public async Task<Guid> Execute(Ride ride)
+    public async Task<Guid> Execute(Guid passengerId, Distance from, Distance to)
     {
-        return Guid.NewGuid();
+        ride = await _rideRepository.Create(new Ride(passengerId, from, to));
+        return ride.Id;
     }
 }
