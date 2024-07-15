718836297aa8a996d9de6d370d6098856aee213a
Matheus Batista <mbatista.sarti@gmail.com>
2024-07-15 08:39:26 -0300

    :sparkles: create use case GetRide

--- RideApp/src/RideApp.Domain/UseCases/GetRide.cs
+++ RideApp/src/RideApp.Domain/UseCases/GetRide.cs
@@ -1,6 +1,14 @@
+using RideApp.Domain.Entities;
+using RideApp.Domain.Interfaces;
+
 namespace RideApp.Domain.UseCases;
 
-public class GetRide
+public class GetRide (IRideRepository rideRepository) : IUseCase<Guid, Ride>
 {
-    
+    private readonly IRideRepository _rideRepository = rideRepository;
+
+    public async Task<Ride> Execute(Guid rideId)
+    {
+        return await _rideRepository.GetById(rideId);
+    }
 }
