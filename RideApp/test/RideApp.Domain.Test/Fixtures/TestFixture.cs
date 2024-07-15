c0195e1918290000d14f58d4415a9a7a7908ba10
Matheus Batista <mbatista.sarti@gmail.com>
2024-07-15 08:43:07 -0300

    :sparkles: add services to TestFixture

--- RideApp/test/RideApp.Domain.Test/Fixtures/TestFixture.cs
+++ RideApp/test/RideApp.Domain.Test/Fixtures/TestFixture.cs
@@ -12,20 +12,27 @@ public class TestFixture : IDisposable
     public AppDbContext DbContext { get; }
     public SignUp SignUp { get; }
     public RequestRide RequestRide { get; }
+    public GetRide GetRide { get; }
+    public AcceptRide AcceptRide { get; }
 
     public TestFixture()
     {
         //SERVICE COLLECTION
         var serviceCollection = new ServiceCollection();
+        
         //Context
         serviceCollection.AddDbContext<AppDbContext>(builder =>
         {
             builder.UseNpgsql("Host=localhost;Port=5432;Database=cccc;Username=postgres;Password=148036");
         });
+        
         //UseCases
         serviceCollection.AddScoped<SignUp>();
         serviceCollection.AddScoped<GetAccount>();
         serviceCollection.AddScoped<RequestRide>();
+        serviceCollection.AddScoped<GetRide>();
+        serviceCollection.AddScoped<AcceptRide>();
+        
         //Repositories
         serviceCollection.AddTransient<IAccountRepository, AccountRepository>();
         serviceCollection.AddTransient<IRideRepository, RideRepository>();
@@ -35,11 +42,13 @@ public class TestFixture : IDisposable
         DbContext = serviceProvider.GetService<AppDbContext>()!;
         SignUp = serviceProvider.GetService<SignUp>()!;
         RequestRide = serviceProvider.GetService<RequestRide>()!;
+        AcceptRide = serviceProvider.GetService<AcceptRide>();
+        GetRide = serviceProvider.GetService<GetRide>()!;
     }
     
-    public void Dispose()
+    public async void Dispose()
     {
-        DbContext.SaveChanges();
-        DbContext.Dispose();
+        await DbContext.SaveChangesAsync();
+        await DbContext.DisposeAsync();
     }
 }
