42dbdd6e6a1f87318d91ca058f4a24a9cbc41dcd
Matheus Batista <mbatista.sarti@gmail.com>
2024-07-15 08:41:02 -0300

    :sparkles: add to ride repository method to get whether there is uncompleted ride by passenger

--- RideApp/src/RideApp.Data/Repositories/RideRepository.cs
+++ RideApp/src/RideApp.Data/Repositories/RideRepository.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using RideApp.Data.Context;
 using RideApp.Domain.Entities;
 using RideApp.Domain.Interfaces;
 
 namespace RideApp.Domain;
 
-public class RideRepository : Repository<Ride>, IRideRepository
+public class RideRepository(AppDbContext context) : Repository<Ride>(context), IRideRepository
 {
-    public RideRepository(AppDbContext context) : base(context)
-    { }
+    public async Task<bool> HasUncompletedRide(Guid passengerId)
+    {
+        var uncompletedRide = await Context.Rides.FromSqlRaw("SELECT * FROM \"Rides\" WHERE \"Status\" != 'completed' AND \"PassengerId\" = {0}", passengerId).ToListAsync();
+        return uncompletedRide.Any();
+        // return await Context.Rides.AnyAsync(ride => ride.Status != "completed");
+    }
 }
